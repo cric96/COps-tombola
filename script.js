35c54191be237b9ecb490c69cf0378c9017194a9
Francesca Neri <104086181+CescaNeri@users.noreply.github.com>
2023-12-03 14:38:13 +0100

    update script

--- script.js
+++ script.js
@@ -1,35 +1,29 @@
 // script.js
 
-const imageFolder = 'images'; // Folder where your images are stored
-const imagesPerRow = 10; // Number of images to display per row
+const imageFolder = 'images';
 let selectedImages = [];
 let images = [];
 
 function generateRandomImage() {
     if (selectedImages.length === 0) {
-        // Load images on the first click
         loadImages();
     }
 
     const latestImageContainer = document.getElementById('latestImageContainer');
     const randomImage = getRandomImage();
 
-    // Display the latest selected image
     latestImageContainer.innerHTML = `<img src="${randomImage}" alt="Latest Selected Image">`;
 
-    // Display the selected images above the button
     const selectedImagesContainer = document.getElementById('selectedImages');
     selectedImagesContainer.innerHTML += `<img src="${randomImage}" alt="Selected Image">`;
 
-    // Add the selected image to the array to avoid duplicates
     selectedImages.push(randomImage);
 }
 
 function getRandomImage() {
     const availableImages = getAvailableImages();
     const randomIndex = Math.floor(Math.random() * availableImages.length);
-    const randomImage = availableImages.splice(randomIndex, 1)[0];
-    return randomImage;
+    return availableImages[randomIndex];
 }
 
 function getAvailableImages() {
@@ -37,21 +31,10 @@ function getAvailableImages() {
 }
 
 function loadImages() {
-    // Fetch the list of images from the folder
-    fetchImagesList()
-        .then(imagesList => {
-            images = imagesList;
+    fetch('images-list.json')
+        .then(response => response.json())
+        .then(data => {
+            images = data.images.map(image => `${imageFolder}/${image}`);
         })
-        .catch(error => {
-            console.error('Error loading images:', error);
-        });
-}
-
-function fetchImagesList() {
-    return new Promise((resolve, reject) => {
-        fetch(`images-list.json`) // Assuming you have a file listing your images
-            .then(response => response.json())
-            .then(data => resolve(data.images.map(image => `${imageFolder}/${image}`)))
-            .catch(error => reject(error));
-    });
+        .catch(error => console.error('Error loading images:', error));
 }
